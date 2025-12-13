25674c14dbf0b41909db9e1a0b118f21e12f1461
Ritik5629 <ritikrana2804@gmail.com>
2025-12-13 14:33:37 +0530

    Item Widget Created

---
+++ lib/item_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} pressed");
+        },
+        leading: Image.network(
+          item.image,
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price}",
+
+          style: TextStyle(
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
